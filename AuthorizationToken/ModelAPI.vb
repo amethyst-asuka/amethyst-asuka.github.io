1371b04d14da1b6573c6e9206e7b9496f5a3110a
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-07-23 02:55:14 +0800

    not works well of the neuron network

--- AuthorizationToken/ModelAPI.vb
+++ AuthorizationToken/ModelAPI.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Serialization.JSON
 
 Module ModelAPI
 
@@ -11,5 +12,41 @@ Module ModelAPI
             .token = dev.token,
             .uid = dev.uid
         }
+    End Function
+
+    <Extension>
+    Public Function NewToken(test As neural_tokens, index As Integer, user As Long) As Storage.MySql.neural_tokens
+        Return New Storage.MySql.neural_tokens With {
+            .key = index,
+            .t1 = test.t1,
+            .t2 = test.t2,
+            .t3 = test.t3,
+            .t4 = test.t4,
+            .t5 = test.t5,
+            .user = user
+        }
+    End Function
+
+    <Extension>
+    Public Function ToArray(x As Storage.MySql.neural_tokens) As Double()
+        Return New Double() {x.t1, x.t2, x.t3, x.t4, x.t5}
+    End Function
+
+    <Extension>
+    Public Function ToArray(x As neural_tokens) As Double()
+        Return New Double() {x.t1, x.t2, x.t3, x.t4, x.t5}
+    End Function
+
+    <Extension>
+    Public Function GetValue(x As Storage.MySql.neural_tokens) As Double
+        Select Case x.key
+            Case 0 : Return x.t1
+            Case 1 : Return x.t2
+            Case 2 : Return x.t3
+            Case 3 : Return x.t4
+            Case 4 : Return x.t5
+            Case Else
+                Throw New Exception(x.GetJson)
+        End Select
     End Function
 End Module
---
+++ AuthorizationToken/NeuronTest.vb
@@ -0,0 +1,50 @@
+﻿Imports Microsoft.VisualBasic.DataMining
+Imports Microsoft.VisualBasic.DataMining.NeuralNetwork
+Imports Microsoft.VisualBasic
+
+''' <summary>
+''' Using neuron network to test a authorization.
+''' </summary>
+Public Class NeuronTest
+
+    ReadOnly __server As Server
+
+    Sub New(server As Server)
+        __server = server
+    End Sub
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="token"></param>
+    ''' <returns></returns>
+    ''' <remarks>
+    ''' 为了确保安全，会进行若干次测试，假若通过测试，则会将得分最高的添加进入数据库之中
+    ''' </remarks>
+    Public Function Test(token As neural_tokens(), user As Long) As Boolean
+        Dim SQL As String = String.Format(SQLGetTestByUserId, user)
+        Dim testData As Storage.MySql.neural_tokens() =
+            __server.MySql.Query(Of Storage.MySql.neural_tokens)(SQL)
+        Dim ANN As New Network(5, 10, 1,,, New IFuncs.SigmoidFunction) ' 输入5个数据计算出一个最偏爱的数值，共10层网络
+        Dim learn As New TrainingUtils(ANN) With {
+            .TrainingType = TrainingType.Epoch
+        }
+
+        For Each x In testData
+            Call learn.Add(x.ToArray, {x.GetValue})
+        Next
+
+        Call learn.Train()
+
+        Dim result As New List(Of Double)
+
+        For Each x In token
+            result += learn.NeuronNetwork.Compute(x.ToArray)(Scan0)
+        Next
+
+
+    End Function
+
+    Const SQLGetTestByUserId As String = "SELECT * FROM neural_tokens WHERE user={0};"
+
+End Class
